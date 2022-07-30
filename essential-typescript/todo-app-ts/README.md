d9199fb9d46b9a1f0632dfa51ac22a617805e9e1
Or Hasson <hassonor@gmail.com>
2022-07-30 23:52:02 +0200

    finish todo demo with inquirer interactive cmd

---
+++ essential-typescript/todo-app-ts/README.md
@@ -0,0 +1,8 @@
+## How to Run
+
+___
+
+1. `npm i -g typescript`
+2. `npm i`
+3. `tsc`
+4. `node .\dist\index.js`
--- essential-typescript/todo-app-ts/src/index.ts
+++ essential-typescript/todo-app-ts/src/index.ts
@@ -1,21 +1,85 @@
 import {TodoItem} from "./todoItem";
 import {TodoCollection} from "./TodoCollection";
+import * as inquirer from "inquirer";
 
 let todos: TodoItem[] = [
     new TodoItem(1, "Buy Me Flowers"), new TodoItem(2, "Get New Shoes"),
-    new TodoItem(2, "Buy Hapoel Tel Aviv tickets for the Derby"), new TodoItem(4, "Call Shira ex.", true)];
+    new TodoItem(2, "Buy Hapoel Tel Aviv tickets for the Derby"), new TodoItem(3, "Call Shira ex.", true)];
 
 
 let collection: TodoCollection = new TodoCollection("Or Hasson", todos);
+let showCompleted = true;
 
-console.clear();
-console.log(`${collection.userName}'s Todo List`);
+function displayTodoList(): void {
+    console.log(`${collection.userName}'s Todo List ${collection.getItemCount().incomplete} items to do.`);
+    collection.getTodoItems(showCompleted).forEach(item => item.printDetails());
+}
 
-let newId: number = collection.addTodo("Go to Gym");
-let todoItem: TodoItem = collection.getTodoById(newId);
-todoItem.printDetails();
+enum Commands {
+    Add = "Add New Task",
+    Complete = "Complete Task",
+    Toggle = "Show/Hide Completed",
+    Purge = "Remove Completed Tasks",
+    Quit = "Quit"
+}
 
+function promptAdd(): void {
+    console.clear();
+    inquirer.prompt({type: "input", name: "add", message: "Enter task:"})
+        .then(answers => {
+            if (answers["add"] !== "") {
+                collection.addTodo(answers["add"]);
+            }
+            promptUser();
+        })
+}
 
+function promptComplete(): void {
+    console.clear();
+    inquirer.prompt({
+        type: "checkbox", name: "complete",
+        message: "Mark Tasks Complete",
+        choices: collection.getTodoItems(showCompleted).map(item =>
+            ({name: item.task, value: item.id, checked: item.complete}))
+    }).then(answers => {
+        let completedTasks = answers["complete"] as number[];
+        collection.getTodoItems(true).forEach(item =>
+            collection.markComplete(item.id,
+                completedTasks.find(id => id === item.id) != undefined));
+        promptUser();
+    })
+}
 
+function promptUser(): void {
+    console.clear();
+    displayTodoList();
+    inquirer.prompt({
+        type: "list",
+        name: "command",
+        message: "Choose option",
+        choices: Object.values(Commands),
+    }).then(answers => {
+        switch (answers["command"]) {
+            case Commands.Toggle:
+                showCompleted = !showCompleted;
+                promptUser();
+                break;
+            case Commands.Add:
+                promptAdd();
+                break;
+            case Commands.Complete:
+                if (collection.getItemCount().incomplete > 0) {
+                    promptComplete();
+                } else {
+                    promptUser();
+                }
+                break;
+            case Commands.Purge:
+                collection.removeComplete();
+                promptUser();
+                break;
+        }
+    })
+}
 
-
+promptUser();
