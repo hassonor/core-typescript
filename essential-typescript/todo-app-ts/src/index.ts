ff2be45ae063ae46e00490c7231c463345729a56
Or Hasson <hassonor@gmail.com>
2022-07-30 21:21:12 +0200

    continue with todoItem demo

--- essential-typescript/todo-app-ts/src/index.ts
+++ essential-typescript/todo-app-ts/src/index.ts
@@ -1,2 +1,21 @@
+import {TodoItem} from "./todoItem";
+import {TodoCollection} from "./TodoCollection";
+
+let todos: TodoItem[] = [
+    new TodoItem(1, "Buy Me Flowers"), new TodoItem(2, "Get New Shoes"),
+    new TodoItem(2, "Buy Hapoel Tel Aviv tickets for the Derby"), new TodoItem(4, "Call Shira ex.", true)];
+
+
+let collection: TodoCollection = new TodoCollection("Or Hasson", todos);
+
 console.clear();
-console.log("Adam's Todo List");
+console.log(`${collection.userName}'s Todo List`);
+
+let newId: number = collection.addTodo("Go to Gym");
+let todoItem: TodoItem = collection.getTodoById(newId);
+todoItem.printDetails();
+
+
+
+
+
